d9b8fc025aee692689043f979d0dd5cf93c31cf7
Samuel Lee <samueljklee@gmail.com>
2018-01-21 12:10:30 -0800

    Uploaded codes

---
+++ Arduino/ECG/ECG.ino
@@ -0,0 +1,30 @@
+
+#define LMINUS D6
+#define LPLUS D7
+#define AnalogPin A0
+
+void setup() {
+  pinMode(LMINUS,INPUT);
+  pinMode(LPLUS,INPUT);
+  pinMode(AnalogPin, OUTPUT);
+
+}
+
+void loop() {
+  Serial.begin(9600);
+  //Debugging
+  Serial.println(digitalRead(LMINUS));
+  Serial.println(digitalRead(LPLUS));
+  Serial.println(analogRead(AnalogPin));
+  
+  if((digitalRead(LMINUS)==1)||(digitalRead(LPLUS)==1)){
+      Serial.println("!");
+  }
+  else{
+      Serial.println(analogRead(AnalogPin));
+  }
+  delay(100);
+  Serial.end();
+}
+
+
