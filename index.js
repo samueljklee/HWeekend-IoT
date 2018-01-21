bc3810c322dd7d676fa0c1a62a3ad5589881e870
Multezem Kedir <kedirmultezem@gmail.com>
2018-01-20 21:15:04 -0800

    json changed

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require("path");
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
 });
-server.listen(3000, function () {
+
+app.post('/postjson', function(req, res) {
+	var time  = req.body.Time;
+	var hum  = req.body.Humidity;
+	var temp = req.body.Temperature;
+	var heat = req.body.HeatIndex;
+	console.log(time);
+	console.log(hum);
+	console.log(temp);
+	console.log(heat);
+});
+
+app.get('/:data', function(req, res) {
+  var data  = req.param("data");
+  console.log(data);// 1234
+  res.send("Data is" + req.param("data"));
+});
+ 
+app.listen(3000, function () {
   console.log("== Server listening on port 3000");
 });
